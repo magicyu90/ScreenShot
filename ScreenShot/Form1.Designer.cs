7f0a8b5ad3ab382c23d241f33beec5dd12f196ec
magicyu90 <magicyu90@gmail.com>
2017-07-21 01:05:17 +0800

    Add project files.

---
+++ ScreenShot/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ScreenShot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(78, 211);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始截屏";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(238, 211);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(102, 33);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止截屏";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 397);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "截屏工具";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenShot/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenShot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += Timer1_Tick;
+            var intervalMinute = int.Parse(ConfigurationManager.AppSettings["intervalMinutes"]);
+            timer1.Interval = 60 * 1000 * intervalMinute;
+        }
+
+        private static readonly string screenShotDir = @"D://screenshots//";
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            Task.Factory.StartNew(() =>
+            {
+
+            }).ContinueWith(task =>
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            SaveImage();
+            timer1.Start();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            SaveImage();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Task.Factory.StartNew(() =>
+            {
+
+            }).ContinueWith(task =>
+            {
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void SaveImage()
+        {
+            if (!Directory.Exists(screenShotDir))
+            {
+                Directory.CreateDirectory(screenShotDir);
+            }
+            var currentDayStr = DateTime.Now.ToString("yyyyMMdd");
+            var dayDir = screenShotDir + currentDayStr + "_Screen" + "//";
+            if (!Directory.Exists(dayDir))
+            {
+                Directory.CreateDirectory(dayDir);
+            }
+
+            var name = DateTimeToUnixTimestamp(DateTime.Now);
+            var filePath = dayDir + name + ".jpg";
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                bitmap.Save(filePath, ImageFormat.Jpeg);
+            }
+
+        }
+
+        private long DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (Int64)(TimeZoneInfo.ConvertTimeToUtc(dateTime) -
+                   new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+        }
+    }
+}
